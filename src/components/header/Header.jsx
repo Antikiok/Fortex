19dc154d6ad89b60c1bc581aa1909e4584d7a2cc
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-11-16 22:47:26 +0200

    aded burger and logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
+import { BsFacebook } from 'react-icons/Bs';
+import { AiOutlineGoogle, AiOutlineYoutube } from 'react-icons/Ai';
 import Search from './Search'
 import Cart from './Cart'
 import './header.scss';
 
-const Header = () => {
-    console.log('s');
+const testArr = ['Автомобили', 'Недвижемость', 'Победители']
 
+const Header = ({ categoriesArray = testArr }) => {
+  console.log('s');
+  const handleBurgerClick = () => console.log('burger clicked');
   return (
     <header className='header'>
-      
-      <div className="menu-block">
-         <div className="line-wrapper">
-            <Search />
-          <Cart />
-         </div>
-     
-
-        
+      <div className='header__top-line'>
+        <div className="header__top-line_wrapper">
+          <div className="burger" onClick={handleBurgerClick}><span></span></div>
+          <a href="#" className="logo">FORTEX</a>
+        </div>
+        <div className="header__socials">
+          <a href="#"><BsFacebook />
+          </a>
+          <a href="#">
+            <AiOutlineGoogle />
+          </a>
+          <a href="#">
+            <AiOutlineYoutube />
+          </a>
+        </div>
       </div>
+      <div className="menu-block">
+        <div className="line-wrapper">
+          <Search />
+          <Cart />
+        </div>
+        <div className='categories'>
+          <button className="categories__btn">←</button>
+          <ul className='categories__list'>
+            {/* change index to item ID with valid data */}
+            {categoriesArray.map((item, index) => <li key={index} className="categories__item">{item}</li>)}
+          </ul>
+          <button className="categories__btn">→</button>
+        </div>
+      </div>
+
     </header>
   );
 };
