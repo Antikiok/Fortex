1bfeee968baba874af682bd5c5526598f6447e29
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-11-16 15:34:55 +0200

    initial setup

---
+++ src/Fortex.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './components/header/Header.jsx';
+
+const Fortex = () => {
+  console.log('main page');
+  return <Header />;
+};
+
+export default Fortex;
